dd48199de5fd04793eeaff9ea6d0587eb042b5a8
Ryan Shin <changyeolshin@gmail.com>
2017-12-15 09:49:41 -0800

    Added the store and connected it to the ChoiceInput component. #1

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { Navigation } from 'react-native-navigation'
+import { Provider } from 'react-redux'
 import startMainTabs from './src/screens/MainTabs/startMainTabs'
 import ChoiceInputScreen from './src/screens/ChoiceInput/ChoiceInput'
 import ChoiceListScreen from './src/screens/ChoiceList/ChoiceList'
+import configureStore from './src/store/configureStore'
+
+const store = configureStore()
 
 Navigation.registerComponent(
   'tbd.ChoiceListScreen',
@@ -10,7 +14,9 @@ Navigation.registerComponent(
 
 Navigation.registerComponent(
   'tbd.ChoiceInputScreen',
-  () => ChoiceInputScreen
+  () => ChoiceInputScreen,
+  store,
+  Provider
 )
 
 startMainTabs()
