b8a8f300b897ee17a043379eb501c48d07b78db6
Ryan Shin <changyeolshin@gmail.com>
2017-12-14 18:27:17 -0800

    Created a ChoiceInput screen component. #1

---
+++ src/screens/ChoiceInput/ChoiceInput.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class ChoiceInputScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>ChoiceInput</Text>
+      </View>
+    )
+  }
+}
+
+export default ChoiceInputScreen
