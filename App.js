beaeee8292a452f2482895d5e2888b60b8c536d6
Ryan Shin <changyeolshin@gmail.com>
2017-12-15 09:36:56 -0800

    Let the App component render the MainTabs component. #1

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { Navigation } from 'react-native-navigation'
 import startMainTabs from './src/screens/MainTabs/startMainTabs'
-import ChoiceInputScreen from './src/screens/ChoiceList/ChoiceList'
-import ChoiceListScreen from './src/screens/ChoiceInput/ChoiceInput'
+import ChoiceInputScreen from './src/screens/ChoiceInput/ChoiceInput'
+import ChoiceListScreen from './src/screens/ChoiceList/ChoiceList'
 
 Navigation.registerComponent(
   'tbd.ChoiceListScreen',
