eb93f32cc9b38c553e54a398abbdc9a908169d9e
Ryan Shin <changyeolshin@gmail.com>
2017-12-14 18:25:45 -0800

    Provided react-native-navigations by registering the ChoiceInput and ChoiceList components. #1

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Navigation } from 'react-native-navigation'
+import startMainTabs from './src/screens/MainTabs/startMainTabs'
+import ChoiceInputScreen from './src/screens/ChoiceList/ChoiceList'
+import ChoiceListScreen from './src/screens/ChoiceInput/ChoiceInput'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+Navigation.registerComponent(
+  'tbd.ChoiceListScreen',
+  () => ChoiceListScreen
+)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+Navigation.registerComponent(
+  'tbd.ChoiceInputScreen',
+  () => ChoiceInputScreen
+)
+
+startMainTabs()
