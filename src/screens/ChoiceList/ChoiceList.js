0abe4bd9c8e8557289c1071b787e48313819c2a7
Ryan Shin <changyeolshin@gmail.com>
2017-12-17 15:16:55 -0800

    Imported the ChoiceItem component onto the ChoiceList component and connect the choices states from redux to the ChoiceList container. #3

--- src/screens/ChoiceList/ChoiceList.js
+++ src/screens/ChoiceList/ChoiceList.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  Text,
+  ImageBackground
+} from 'react-native'
+import uuid from 'uuid-v4'
+import ChoiceItem from '../../components/ChoiceItem/ChoiceItem'
+import backgroundImage from '../../assets/background.jpg'
 
 class ChoiceListScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>ChoiceList</Text>
-      </View>
+      <ImageBackground
+        source={backgroundImage}
+        style={styles.listContainer}
+      >
+        <FlatList
+          data={this.props.choices}
+          keyExtractor={() => uuid()}
+          renderItem={info => (
+            <ChoiceItem
+              key={uuid()}
+              choice={info.item.choice}
+              author={info.item.author}
+            />
+        )}
+        />
+    </ImageBackground>
     )
   }
 }
 
-export default ChoiceListScreen
+const mapStateToProps = state => {
+  return {
+    choices: state.choices.choices
+  }
+}
+
+const styles = StyleSheet.create({
+  listContainer: {
+    flex: 1,
+    marginTop: 5
+  }
+})
+
+export default connect(mapStateToProps, null)(ChoiceListScreen)
