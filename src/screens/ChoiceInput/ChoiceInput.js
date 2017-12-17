123c2f91f202af8aef0bfc7c9349787a229f568e
Ryan Shin <changyeolshin@gmail.com>
2017-12-16 20:13:17 -0800

    Connect the choices state from redux to the ChoiceInput component. #1

--- src/screens/ChoiceInput/ChoiceInput.js
+++ src/screens/ChoiceInput/ChoiceInput.js
@@ -1,14 +1,133 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  ImageBackground,
+  TouchableOpacity
+} from 'react-native'
+import DefaultInput from '../../components/UI/DefaultInput/DefaultInput'
+import ButtonWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground'
+import validate from '../../utility/validate'
+import { addChoice } from '../../store/actions/index'
+import backgroundImage from '../../assets/background.jpg'
 
 class ChoiceInputScreen extends Component {
+  state = {
+    controls: {
+      choice: {
+        value: '',
+        valid: false,
+        validationRules: {
+          isNotEmpty: true,
+          minLength: 3
+        },
+        touched: false,
+      },
+      author: {
+        value: '',
+        valid: false,
+        validationRules: {
+          isNotEmpty: true,
+          minLength: 3
+        },
+        touched: false
+      }
+    },
+    disabled: true
+  }
+
+  componentDidMount() {
+    if (!this.state.controls.choice.valid && !this.state.controls.author.valid) {
+      this.setState({ disabled: false })
+    }
+  }
+
+  updateInputState = (type, value) => {
+    this.setState(prevState => {
+      return {
+        controls: {
+          ...prevState.controls,
+          [type]: {
+            ...prevState.controls[type],
+            value: value,
+            valid: validate(value, prevState.controls[type].validationRules),
+            touched: true
+          }
+        }
+      }
+    })
+  }
+
+  addChoiceHandler = (choice, author) => {
+    const choiceData = {
+      choice: choice,
+      author: author
+    }
+    this.props.addChoiceToList(choiceData)
+  }
+
   render() {
     return (
-      <View>
-        <Text>ChoiceInput</Text>
-      </View>
+      <ImageBackground
+        source={backgroundImage}
+        style={styles.imageBackground}
+      >
+        <DefaultInput
+          placeholder='Please enter a choice.'
+          value={this.state.controls.choice.value}
+          valid={this.state.controls.choice.valid}
+          onChangeText={value => this.updateInputState('choice', value)}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <DefaultInput
+          value={this.state.controls.author.value}
+          placeholder='Please enter the author of the choice.'
+          valid={this.state.controls.author.valid}
+          onChangeText={value => this.updateInputState('author', value)}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <ButtonWithBackground
+          disabled={this.state.disabled}
+          color="#5f9f89"
+          onPress={() => {
+            this.addChoiceHandler(
+              this.state.controls.choice.value,
+              this.state.controls.author.value
+            )
+          }}
+        >
+          Submit
+        </ButtonWithBackground>
+        <Text>{JSON.stringify(this.props.choices)}</Text>
+      </ImageBackground>
     )
   }
 }
 
-export default ChoiceInputScreen
+const styles = StyleSheet.create({
+  imageBackground: {
+    width: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+const mapStateToProps = state => {
+  return {
+    choices: state.choices.choices
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addChoiceToList: choiceData => dispatch(addChoice(choiceData))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChoiceInputScreen)
