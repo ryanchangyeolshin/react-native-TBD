cc5bd79e266eef71267a387794a192c720eeb405
Ryan Shin <changyeolshin@gmail.com>
2017-12-17 16:22:55 -0800

    Created the clear button and added the deleteAllChoices functionality to the button when pressed by the user. Also provided animation of the choices to fade out. #5

--- src/screens/ChoiceList/ChoiceList.js
+++ src/screens/ChoiceList/ChoiceList.js
@@ -5,30 +5,62 @@ import {
   View,
   FlatList,
   Text,
-  ImageBackground
+  ImageBackground,
+  Animated
 } from 'react-native'
 import uuid from 'uuid-v4'
 import ChoiceItem from '../../components/ChoiceItem/ChoiceItem'
-import backgroundImage from '../../assets/background.jpg'
+import ButtonWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground'
+import { deleteAllChoices } from '../../store/actions/index'
+import backgroundImage from '../../assets/background.png'
 
 class ChoiceListScreen extends Component {
+  state = {
+    clearChoicesAnimation: new Animated.Value(1)
+  }
+
+  clearAllChoicesHandler = () => {
+    Animated.timing(this.state.clearChoicesAnimation, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true
+    }).start(() => {
+      this.props.clearAllChoices()
+    })
+  }
+
   render() {
     return (
       <ImageBackground
         source={backgroundImage}
         style={styles.listContainer}
       >
-        <FlatList
-          data={this.props.choices}
-          keyExtractor={() => uuid()}
-          renderItem={info => (
-            <ChoiceItem
-              key={uuid()}
-              choice={info.item.choice}
-              author={info.item.author}
-            />
-        )}
-        />
+        <View style={styles.buttonContainer}>
+          <ButtonWithBackground
+            color='red'
+            onPress={this.clearAllChoicesHandler}
+            disabled={this.props.choices.length === 0}
+          >
+            Clear
+          </ButtonWithBackground>
+        </View>
+        <Animated.View
+          style={{
+            opacity: this.state.clearChoicesAnimation
+          }}
+        >
+          <FlatList
+            data={this.props.choices}
+            keyExtractor={() => uuid()}
+            renderItem={info => (
+              <ChoiceItem
+                key={uuid()}
+                choice={info.item.choice}
+                author={info.item.author}
+              />
+          )}
+          />
+        </Animated.View>
     </ImageBackground>
     )
   }
@@ -40,11 +72,20 @@ const mapStateToProps = state => {
   }
 }
 
+const mapDispatchToProps = dispatch => {
+  return {
+    clearAllChoices: () => dispatch(deleteAllChoices())
+  }
+}
+
 const styles = StyleSheet.create({
   listContainer: {
     flex: 1,
     marginTop: 5
+  },
+  buttonContainer: {
+    alignItems: 'center'
   }
 })
 
-export default connect(mapStateToProps, null)(ChoiceListScreen)
+export default connect(mapStateToProps, mapDispatchToProps)(ChoiceListScreen)
