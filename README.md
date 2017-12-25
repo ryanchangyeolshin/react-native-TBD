c9951da8c577cba95b071c6e029a3eac9e8371c6
Ryan Shin <changyeolshin@gmail.com>
2017-12-24 16:39:29 -0800

    Fixed the android simulator configurations. #11

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # react-native-TBD
 A mobile application for indecisive people who want to be given a decision.
-
-## Compatible for ios and andoid.
--- android/app/src/main/java/com/reactnativetbd/MainApplication.java
+++ android/app/src/main/java/com/reactnativetbd/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativenavigation.NavigationApplication;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,51 +20,46 @@ public class MainApplication extends NavigationApplication {
   //   public boolean getUseDeveloperSupport() {
   //     return BuildConfig.DEBUG;
   //   }
-  // 
+  //
   //   @Override
   //   protected List<ReactPackage> getPackages() {
   //     return Arrays.<ReactPackage>asList(
   //         new MainReactPackage()
   //     );
   //   }
-  // 
+  //
   //   @Override
   //   protected String getJSMainModuleName() {
   //     return "index";
   //   }
   // };
-  // 
+  //
   // @Override
   // public ReactNativeHost getReactNativeHost() {
   //   return mReactNativeHost;
   // }
-  // 
+  //
   // @Override
   // public void onCreate() {
   //   super.onCreate();
   //   SoLoader.init(this, /* native exopackage */ false);
   // }
-  
+
   @Override
   public boolean isDebug() {
     // Make sure you are using BuildConfig from your own application
-    return BuildConfig.DEBUG
+    return BuildConfig.DEBUG;
   }
-  
-  @Override
+
   protected List<ReactPackage> getPackages() {
     return Arrays.<ReactPackage>asList(
       new MainReactPackage(),
       new VectorIconsPackage()
     );
   }
-  
+
   @Override
   public List<ReactPackage> createAdditionalReactPackages() {
-    return getPackages()
+    return getPackages();
   }
-}
-
-public class MainApplication extends NavigationApplication {
-  
 }
