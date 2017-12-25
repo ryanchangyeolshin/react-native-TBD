f78d1441368bc9e355090e9bcd2cbc9ca3166880
Ryan Shin <changyeolshin@gmail.com>
2017-12-25 10:57:49 -0800

    Created a splash screen for the android. #13

--- android/app/src/main/java/com/reactnativetbd/MainActivity.java
+++ android/app/src/main/java/com/reactnativetbd/MainActivity.java
@@ -1,5 +1,26 @@
 package com.reactnativetbd;
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
+  @Override
+  public LinearLayout createSplashLayout() {
+    LinearLayout view = new LinearLayout(this);
+    TextView textView = new TextView(this);
+
+    view.setBackgroundColor(Color.parseColor("#4554b9"));
+    view.setGravity(Gravity.CENTER);
+
+    textView.setTextColor(Color.parseColor("#ffffff"));
+    textView.setText("T.B.D.");
+    textView.setGravity(Gravity.CENTER);
+    textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+    view.addView(textView);
+    return view;
+  }
 }
